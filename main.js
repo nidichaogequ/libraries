078cd0a6a5c7a1074c81359ba4f925f2d6724270
nidichaogequ <liyuanfx502692@yeah.net>
2023-10-28 22:35:00 +0800

    Pop Count Checkpoint

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+let clickCount = 0
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxSize = 300
+let popCount = 0
+
 function inflate() {
+    clickCount++
     var balloonElement = document.getElementById("balloon")
-    balloonElement.style.height = "400px"
-    balloonElement.style.width = "300px"
+    height += inflationRate
+    width += inflationRate
+    if (height >= maxSize) {
+        console.log("Balloon Popped")
+        height = 50
+        width = 30
+    }
+    balloonElement.style.height = height + "px"
+    balloonElement.style.width = width + "px"
+
+    var clickCountElement = document.getElementById("click-count")
+    clickCountElement.innerText = clickCount.toString()
 }
